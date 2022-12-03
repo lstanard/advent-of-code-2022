234ec932670f2f9836861bda6fd57850fcc3d231
Charlie Stanard <louis.stanard@gmail.com>
2022-12-03 13:44:14 -0600

    day 3, part 1 solution

---
+++ src/day-3/part-1.ts
@@ -0,0 +1,26 @@
+import { readInput } from "../utils/readInput";
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+export const getPart1Answer = (inputFile = 'input.txt') => {
+  const input = readInput(__dirname, inputFile);
+  const splitData: string[] = input.split(/\n/);
+
+  const priorityValues = splitData.map((fullContents) => {
+    const allValues = fullContents.split('');
+    const half = Math.floor(allValues.length / 2)
+    const leftHalf = allValues.slice(0, half);
+    const rightHalf = allValues.slice(half, allValues.length);
+
+    let commonValue = 0;
+    for (let index = 0; index < leftHalf.length; index++) {
+      if (rightHalf.includes(leftHalf[index])) {
+        commonValue = alphabet.indexOf(leftHalf[index]) + 1;
+        break;
+      }
+    }
+    return commonValue;
+  });
+
+  return priorityValues.reduce((prev, curr) => prev + curr);
+}
