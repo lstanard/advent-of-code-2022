c7f3cb989c633b3825cfc6b458fa62897f72ffaa
Charlie Stanard <louis.stanard@gmail.com>
2022-12-07 07:59:36 -0600

    day 7, init

---
+++ src/day-7/index.ts
@@ -0,0 +1,47 @@
+import { readInput } from "../utils/readInput";
+
+const parseInput = (inputFile: string) => {
+  const input: string = readInput(__dirname, inputFile);
+  return input.split(/\n/);
+}
+
+const getFileSizes = (input: string[]) => {
+  let pwd: string[] = [];
+  const fileSizes: Record<string, number> = {};
+  for (let i = 0; i < input.length; i++) {
+    const line = input[i];
+    const path = pwd.join();
+    if (line.includes('$ cd')) {
+      const cdDir = line.match(/(?<=cd\s).{1,}/g)!;
+      if (cdDir && cdDir[0] === '..') {
+        pwd.pop();
+      } else {
+        pwd.push(cdDir[0]);
+      }
+    } else if (line.match(/\d+/g)) {
+      const fileSize = line.match(/\d+/g);
+      const fileSizeNum = Number(fileSize && fileSize[0]);
+      fileSizes[path] = (fileSizes[path] ?? 0) + fileSizeNum;
+    }
+  }
+  
+  return fileSizes;
+}
+
+export const getAnswer = (inputFile = 'input.txt', limit = 100000) => {
+  const input = parseInput(inputFile);
+  const fileSizesByDir = getFileSizes(input);
+
+  Object.entries(fileSizesByDir).forEach(([dir, size]) => {
+    const fullPath = dir.split(',');
+    if (fullPath.length > 1) {
+      const parentPath = fullPath.splice(0, fullPath.length - 1).join();
+      fileSizesByDir[parentPath] = fileSizesByDir[parentPath] + size;
+    }
+  });
+
+  return fileSizesByDir;
+}
+
+// console.log(getAnswer());
+console.log(getAnswer('sample-input.txt'));
